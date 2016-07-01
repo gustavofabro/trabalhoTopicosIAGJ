ae0d0d270de77c9d5d454e16e9256f887e807811
Alini <Alini@Alini-pc>
2016-07-01 17:01:37 -0300

    Alterações Finais

--- GRC/src/br/unesc/topicos/grc/inicializar/GerenciadorMain.java
+++ GRC/src/br/unesc/topicos/grc/inicializar/GerenciadorMain.java
@@ -1,3 +1,14 @@
+/*
+Ciência da Computação - UNESC
+Disciplina: Topicos Especias - 5º fase 
+Professor: Gilberto Vieira
+Academicos: Alini Eyng e Gustavo Fabro
+Ano: 2016/07
+
+Tema: GRC (Gerenciamento e relacionamento com o Cliente) - baseado em um sistema de CRM
+_____________________________________________________________________________________________
+*/
+
 package br.unesc.topicos.grc.inicializar;
 
 import javax.swing.JFrame;
